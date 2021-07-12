90fff29636f253a94043ac51a88f58ce82bcd2de
Mukhammadjon Sirojiddinov <mukhammadjon_sirojiddinov@epam.com>
2021-07-12 12:20:46 +0500

    Prettier and eslint configs

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    env: {
+      commonjs: true,
+      es6: true,
+      node: true,
+    },
+    plugins: ['prettier'],
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+      ecmaVersion: 2018,
+    },
+    rules: {
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      'prettier/prettier': ['error'],
+    },
+  }
