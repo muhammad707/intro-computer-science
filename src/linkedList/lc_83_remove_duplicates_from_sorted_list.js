1047cef162530bb961bd9542ff7ae221494b3e5d
Mukhammadjon Sirojiddinov <mukhammadjon.707s@gmail.com>
2022-03-29 00:10:08 +0500

    Leetcode. Linked List 83. Remove duplicates from sorted list

---
+++ src/linkedList/lc_83_remove_duplicates_from_sorted_list.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  if (head === null) {
+    return null
+  }
+  let curr = head.next,
+    prev = head
+
+  while (prev.next !== null) {
+    if (prev.val == curr.val) {
+      prev.next = curr.next
+    } else {
+      prev = prev.next
+    }
+    curr = curr.next
+  }
+  return head
+}
