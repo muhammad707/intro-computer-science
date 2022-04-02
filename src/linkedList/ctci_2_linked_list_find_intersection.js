6cf446a7a73203f36e4e7aff660fa0e8da9e2b77
Mukhammadjon Sirojiddinov <mukhammadjon.707s@gmail.com>
2022-04-02 22:00:32 +0500

    Leetcode: 160. Intersection of two Linked lists

---
+++ src/linkedList/ctci_2_linked_list_find_intersection.js
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  let res1 = getTailAndSize(headA)
+  let res2 = getTailAndSize(headB)
+
+  if (res1.tail !== res2.tail) {
+    return null
+  }
+
+  let shorter = res1.size <= res2.size ? headA : headB
+  let longer = res1.size > res2.size ? headA : headB
+  console.log(shorter)
+  console.log(longer)
+  longer = getKthNode(longer, Math.abs(res1.size - res2.size))
+
+  while (shorter !== longer) {
+    shorter = shorter.next
+    longer = longer.next
+  }
+
+  return longer
+}
+
+function getTailAndSize(head) {
+  if (head === null) {
+    return null
+  }
+
+  let size = 1,
+    curr = head
+
+  while (curr.next !== null) {
+    size++
+    curr = curr.next
+  }
+
+  return {
+    size,
+    tail: curr,
+  }
+}
+
+function getKthNode(head, k) {
+  let curr = head
+  while (k > 0 && curr !== null) {
+    curr = curr.next
+    k--
+  }
+  return curr
+}
